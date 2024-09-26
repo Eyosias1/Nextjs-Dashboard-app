ff849e54d7486d9d5483ceaa99ffcc428b1afe7c
Eyosias Woldemichael <eyosias16@gmail.com>
2024-09-26 18:25:48 +0200

    added form handling , improved accessiblity

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-    return <p> Customers Page</p>;
+import  Customer  from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+
+
+export default async function Page() {
+    //const customers =  await Promise.all([fetchCustomers()]);
+    const customers = await fetchFilteredCustomers('')
+    return (
+        <main>
+            <Customer customers={customers}/>
+        </main>
+    )
 }
